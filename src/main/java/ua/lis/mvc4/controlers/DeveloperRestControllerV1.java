40f64d5eb8b8a9780fbfe52921636a2ce783daaf
Oleksandr Lisovich <lisovyhs@gmail.com>
2024-10-05 08:05:05 +0300

    add login page and spring-security to pom.xml

---
+++ src/main/java/ua/lis/mvc4/controlers/DeveloperRestControllerV1.java
@@ -0,0 +1,15 @@
+//package ua.lis.mvc4.controlers;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+////@RequestMapping("/login")
+//public class DeveloperRestControllerV1 {
+//
+//    @GetMapping("")
+//    public String login(){
+//        return "/login";
+//    }
+//}
